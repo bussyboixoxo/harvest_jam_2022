4e8c7d9a54ab5b6a834efd6118b17ec44d1e5ea0
DevFireGrasp <srw987987@gmail.com>
2023-01-29 12:52:10 -0500

    More changes!

--- LD_Harvest/Assets/scripts/GameManager.cs
+++ LD_Harvest/Assets/scripts/GameManager.cs
@@ -4,29 +4,63 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject currentAnimal;
-    
+   
+    public bool squirl;
+    public bool crow;
     public GameObject SquirlL;
     public GameObject SquirlR;
     public GameObject SquirlU;
     public GameObject SquirlD;
+    public GameObject RavenL;
+    public GameObject RavenR;
+    public GameObject RavenU;
+    public GameObject RavenD;
     public GameObject selectedAnimalL;
     public GameObject selectedAnimalR;
     public GameObject selectedAnimalU;
     public GameObject selectedAnimalD;
 
+
+    public void Start()
+    {
+        
+    }
     public void Update()
     {
-       if(currentAnimal = SquirlL)
+        
+       if(squirl)
        {
         selectedAnimalL = SquirlL;
         selectedAnimalR = SquirlR;
         selectedAnimalU = SquirlU;
         selectedAnimalD = SquirlD;
        }
+       if(crow)
+       {
+        selectedAnimalL = RavenL;
+        selectedAnimalR = RavenR;
+        selectedAnimalU = RavenU;
+        selectedAnimalD = RavenD;
+       }
     }
     
     
-    
+    public void UISquirlSelect()
+    {
+        squirl = true;
+        crow = false;
+    }
+    public void UIRavenSelect()
+    {
+        squirl = false;
+        crow = true;
+    }
 
+    public void animalCheck()
+    {
+        if(!squirl && !crow)
+        {
+            
+        }
+    }
 }
